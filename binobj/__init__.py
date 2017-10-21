44db9de83aad25a1302ac4c31450a525c0095583
Diego Argueta <dargueta@users.noreply.github.com>
2017-10-20 22:39:43 -0700

    Add wildcard imports at root.

--- binobj/__init__.py
+++ binobj/__init__.py
@@ -4,6 +4,12 @@ binobj
 
 A Python library for reading and writing structured binary data.
 """
+# pylint: disable=wildcard-import,unused-import
+
+from .errors import *
+from .fields import *
+from .serialization import *
+from .structures import *
 
 __version_info__ = (0, 1, 0)
 __version__ = '.'.join(str(v) for v in __version_info__)
