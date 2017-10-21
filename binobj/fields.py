691f8dde4a067bc6e2d9813e4b38effc373f9326
Diego Argueta <dargueta@users.noreply.github.com>
2017-10-20 22:44:15 -0700

    Fields aren't complex enough to warrant a subpackage...yet.

--- binobj/fields.py
+++ binobj/fields.py
@@ -1,9 +1,4 @@
-"""Commonly-used field definitions provided for convenience.
-
-These are built on top of the base fields defined in :mod:`binobj.fields.bases`.
-For example, :class:`UInt16` is equivalent to ``Integer(n_bytes=2, signed=False)``
-but is more readable and shorter to write.
-"""
+"""Definitions of all field types that can be used in structures."""
 
 import sys
 
@@ -11,7 +6,64 @@ import bitstring
 
 from binobj import errors
 from binobj import varints
-from binobj.fields import bases
+from binobj import serialization
+
+
+class Field(serialization.SerializableScalar):
+    """The base class for all struct fields.
+
+    :param str name:
+        The name of the field.
+    :param bool required:
+        If ``True``, this value *must* be passed to the serializer for a struct.
+        If ``False``, the default value will be used.
+    :param bool allow_null:
+        If ``True`` (the default) then ``None`` is an acceptable value to write
+        for this field.
+    :param bitstring.Bits null_value:
+        A value to use to dump ``None``. When loading, the returned value will
+        be ``None`` if this value is encountered.
+    :param default:
+        The default value to use if a value for this field isn't passed to the
+        struct for serialization. If ``required`` is ``False`` and no default is
+        given, null bytes will be used to fill the space required.
+    :param bool discard:
+        When deserializing, don't include this field in the returned results.
+    """
+    def __init__(self, *, name=None, required=True, allow_null=True,
+                 null_value=serialization.DEFAULT, discard=False, **kwargs):
+        if not allow_null and null_value is serialization.DEFAULT:
+            null_value = serialization.UNDEFINED
+
+        self.name = name
+        self.required = required
+        self.discard = discard
+
+        # The following fields are set by the struct metaclass after the field
+        # is instantiated.
+
+        #: A weak reference to the `Struct` class containing this field.
+        self.struct_class = None    # type: binobj.structures.Struct
+
+        #: The zero-based index of the field in the struct.
+        self.index = None   # type: int
+
+        #: The zero-based bit offset of the field in the struct. If the offset
+        #: can't be computed (e.g. it's preceded by a variable-length field),
+        #: this will be ``None``.
+        self.offset = None  # type: int
+
+        # Some arguments and attributes may be references rather than actual
+        # parameters. We must bind those references to this field.
+        for item in vars(self):
+            if isinstance(item, serialization.ValueOf):
+                item.bind(self)
+
+        super().__init__(allow_null=allow_null, null_value=null_value, **kwargs)
+
+    def __str__(self):
+        return '%s::%s(name=%r)' % (
+            (self.struct_class.__name__, type(self).__name__, self.name))
 
 
 _BIT_VALUE_ATTR = {
@@ -22,11 +74,11 @@ _BIT_VALUE_ATTR = {
 }
 
 
-class Bytes(bases.Field):   # pylint: disable=too-few-public-methods
+class Bytes(Field):   # pylint: disable=too-few-public-methods
     """Raw binary data."""
 
 
-class Integer(bases.Field):
+class Integer(Field):
     """An integer.
 
     :param str endian:
@@ -191,7 +243,7 @@ class UInt64(Int64):
         super().__init__(signed=False, **kwargs)
 
 
-class String(bases.Field):
+class String(Field):
     """A fixed-length string.
 
     :param str encoding:
--- binobj/fields/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Definitions of all field types that can be used in structures."""
-
-
-from binobj.fields.bases import *   # pylint: disable=wildcard-import
-from binobj.fields.common import *  # pylint: disable=wildcard-import
