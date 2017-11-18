332b973b4f6a5202027e4a4f6de05350e245f94f
Diego Argueta <dargueta@users.noreply.github.com>
2017-11-17 22:39:07 -0800

    Simplify package dependencies.

--- setup.py
+++ setup.py
@@ -24,16 +24,10 @@ setuptools.setup(
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
     install_requires=[
-        'bitstring>=3.1',
-        'enum34>=1.1;python_version<"3.4"',
+        'bitstring~=3.1',
+        'enum34~=1.1; python_version<"3.4"',
     ],
     python_requires='>=3.3',
-    tests_require=[
-        'bumpversion>=0.5.0',
-        'tox>=2.0',
-        'tox-pyenv>=1.0',
-    ],
     packages=setuptools.find_packages(
         exclude=['tests', '*.tests', '*.tests.*', 'docs']),
 )
-
