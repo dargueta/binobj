9d41d663d4bf9df496bdc49a46f7f35fa331afa6
Diego Argueta <dargueta@users.noreply.github.com>
2017-12-16 12:32:44 -0800

    Move all configuration stuff to setup.cfg

--- setup.py
+++ setup.py
@@ -1,33 +1,4 @@
 import setuptools
 
 
-setuptools.setup(
-    name='binobj',
-    version='0.1.0',
-    description='A Python library for reading and writing structured binary data.',
-    long_description='file: README.rst',
-    author='Diego Argueta',
-    author_email='dargueta@users.noreply.github.com',
-    url='https://www.github.com/dargueta/binobj',
-    license='BSD',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-    install_requires=[
-        'bitstring~=3.1',
-        'enum34~=1.1; python_version<"3.4"',
-    ],
-    python_requires='>=3.3',
-    packages=setuptools.find_packages(
-        exclude=['tests', '*.tests', '*.tests.*', 'docs']),
-)
+setuptools.setup(setup_requires=['setuptools>=30.1.0'])
