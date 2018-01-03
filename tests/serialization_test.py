7463ea9d09f226ef2ba68bf547f93491e9133e03
Diego Argueta <dargueta@users.noreply.github.com>
2018-01-03 09:43:01 -0600

    Fix renamed variable, add more tests.

--- tests/serialization_test.py
+++ tests/serialization_test.py
@@ -3,7 +3,7 @@
 import pytest
 
 import binobj
-from binobj.serialization import gather_options_for_class, _PREDEFINED_KWARGS
+from binobj.serialization import gather_options_for_class, _DEFAULT_OPTIONS
 
 
 class BaseClassWithOptions(binobj.Struct):
@@ -37,7 +37,7 @@ class GrandchildWithOverrides(ChildClassWithOptions):
 ))
 def test_gather_options__single_class(class_object, expected):
     """Basic test for single classes that have options."""
-    expected.update(_PREDEFINED_KWARGS)
+    expected.update(_DEFAULT_OPTIONS)
     assert gather_options_for_class(class_object) == expected
 
 
@@ -55,7 +55,7 @@ def test_gather_options__inherits__with_additional_options():
         'bar': 'baz',
         'child': True
     }
-    expected.update(_PREDEFINED_KWARGS)
+    expected.update(_DEFAULT_OPTIONS)
     assert gather_options_for_class(ChildClassWithOptions) == expected
 
 
@@ -66,7 +66,7 @@ def test_gather_options__inherits__with_overrides():
         'bar': 'baz',
         'child': True
     }
-    expected.update(_PREDEFINED_KWARGS)
+    expected.update(_DEFAULT_OPTIONS)
     assert gather_options_for_class(GrandchildWithOverrides) == expected
 
 
@@ -84,3 +84,23 @@ def test_gather_options__field_inherits_struct():
     assert struct.field.__options__['blah'] == 123
     assert struct.field.__options__['kw1'] == 1
     assert struct.field.__options__['kw2'] == 2
+
+
+class StructWithFieldOverrides(binobj.Struct):
+    class Options:
+        endian = 'big'
+
+    big = binobj.Int32()    # Should be big-endian
+    little = binobj.Int32(endian='little')
+
+
+def test_gather_options__field_overrides_struct():
+    struct = StructWithFieldOverrides()
+    expected_big = {'endian': 'big'}
+    expected_little = {'endian': 'little'}
+
+    expected_big.update(_DEFAULT_OPTIONS)
+    expected_little.update(_DEFAULT_OPTIONS)
+
+    assert struct.big.__options__ == expected_big
+    assert struct.little.__options__ == expected_little
