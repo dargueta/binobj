80ecb190ae52861317190bd376b13a41e0f0510a
Diego Argueta <620513-dargueta@users.noreply.github.com>
2018-02-02 22:20:20 -0800

    Manual Python version check for pip < 9.0

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 import setuptools
+import sys
+
+
+# Thwart installation for unsupported versions of Python. `pip` didn't start enforcing `python_requires` until 9.0.
+if sys.version_info < (3, 3):
+    raise EnvironmentError('Unsupported Python version: ' + sys.version)
 
 
 setuptools.setup(setup_requires=['setuptools>=30.1.0'])
