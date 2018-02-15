f1bf55b8948c94e165773d0aefdcc0b3bc667579
Diego Argueta <620513-dargueta@users.noreply.github.com>
2018-02-14 22:21:16 -0800

    Move setup.cfg stuff to setup.py to support older setuptools

--- setup.py
+++ setup.py
@@ -6,4 +6,33 @@ import sys
 if sys.version_info < (3, 3):
     raise RuntimeError('Unsupported Python version: ' + sys.version)
 
-setuptools.setup(setup_requires=['setuptools>=30.1.0'])
+
+setuptools.setup(
+    author='Diego Argueta',
+    author_email='dargueta@users.noreply.github.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    description='A Python library for reading and writing structured binary data.',
+    install_requires=[
+        'enum34>=1.0; python_version<"3.4"',
+    ],
+    license='BSD 3-Clause License',
+    long_description='file: README.rst',
+    name='binobj',
+    python_requires='>=3.3',
+    packages=setuptools.find_packages(
+        exclude=['docs', 'tests', '*.tests', '*.tests.*']),
+    url='https://www.github.com/dargueta/binobj',
+    version='0.1.0'
+)
