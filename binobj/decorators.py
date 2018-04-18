f9f8177e500ce007bc0c0469737e636a513257fc
Diego Argueta <620513-dargueta@users.noreply.github.com>
2018-04-17 21:57:42 -0700

    Better documentation on the validation decorators.

--- binobj/decorators.py
+++ binobj/decorators.py
@@ -6,14 +6,22 @@ from binobj import validation
 def validates(*field_names):
     """A decorator that marks a method as a validator for one or more fields.
 
-    .. note::
-
-        If you specify multiple validators, the order in which they execute is
-        *not* guaranteed. If you need a specific ordering of checks, you must
-        put them in the same function.
-
     :param field_names:
         One or more names of fields that this validator should be activated for.
+
+    Usage::
+
+        @validates('foo', 'bar')
+        def validator(self, field, value):
+            if value >= 10:
+                raise ValidationError('%r must be in [0, 10).' % field.name,
+                                      field=field)
+
+    .. note::
+
+        If you specify multiple validator methods, the order in which they
+        execute is *not* guaranteed. If you need a specific ordering of checks,
+        you must put them in the same function.
     """
     if not field_names:
         raise TypeError('At least one field name must be given.')
@@ -27,5 +35,22 @@ def validates(*field_names):
 
 
 def validates_struct(func):
-    """A decorator that marks a method as a validator for the entire struct."""
-    return validation.ValidatorMethodWrapper(func, ())
+    """A decorator that marks a method as a validator for the entire struct.
+
+    The method being decorated should take two arguments, the class object and
+    a dictionary of the struct. The validator is invoked right after it's been
+    loaded, or right before it's dumped.
+
+    It's highly inadvisable to modify the dictionary, because it's easy to
+    create invalid results. For example, if a struct has a field giving the
+    length of the array and you change the length of that array, the length field
+    *will not* update to compensate. You must do that yourself.
+
+    Usage::
+
+        @validates_struct
+        def validator(cls, struct_dict):
+            if struct_dict['foo'] % 2 != 0:
+                raise ValidationError("'foo' must be even", field='foo')
+    """
+    return classmethod(validation.ValidatorMethodWrapper(func, ()))
