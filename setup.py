402052587e4e8e6f227fc4fddb0706dff93b45ed
Diego Argueta <620513-dargueta@users.noreply.github.com>
2018-05-13 00:55:00 -0400

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -30,5 +30,5 @@ setuptools.setup(
     packages=setuptools.find_packages(
         exclude=['docs', 'docs.*', 'tests', 'tests.*']),
     url='https://www.github.com/dargueta/binobj',
-    version='0.4.0'
+    version='0.4.1'
 )
