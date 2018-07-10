8a40f4298826d2c93dc7ee55e7912abfa939e4db
Diego Argueta <620513-dargueta@users.noreply.github.com>
2018-07-09 22:01:34 -0700

    Change doc theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,7 +89,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -170,7 +170,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
     (master_doc, 'binobj', 'binobj Documentation',
-     author, 'binobj', 'One line description of project.',
+     author, 'binobj', 'A Python library for reading and writing structured binary data.',
      'Miscellaneous'),
 ]
 
