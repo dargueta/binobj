bb12d45d8626d1093287a28ef08f67c8e10d525f
Diego Argueta <620513-dargueta@users.noreply.github.com>
2019-02-22 07:51:39 -0800

    Bump version: 0.6.0 → 0.6.1

--- binobj/__init__.py
+++ binobj/__init__.py
@@ -7,7 +7,7 @@ A Python library for reading and writing structured binary data.
 
 
 # Do not modify directly; use ``bumpversion`` command instead.
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 __version_info__ = tuple(int(p) for p in __version__.split('.'))
 
 
