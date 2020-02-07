8d60cd58b4172b0f1e563f6995b88d332214bd56
Diego Argueta <620513-dargueta@users.noreply.github.com>
2020-02-06 23:00:07 -0800

    Blacken a few files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 # Thwart installation for unsupported versions of Python. `pip` didn't start
 # enforcing `python_requires` until 9.0.
 if sys.version_info < (3, 5):
-    raise RuntimeError('Unsupported Python version: ' + sys.version)
+    raise RuntimeError("Unsupported Python version: " + sys.version)
 
 
 # TODO (dargueta): Figure out why package detection in setup.cfg stopped working.
